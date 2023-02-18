7d42820ae59bf2b71533f4e2dc5eb2f379624385
Leon7422 <y.kurduban98@gmail.com>
2023-02-18 10:21:48 +0200

    add base firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+import "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDL4D4xvd6P4jz0UWp1IrOTJX_N6qje6-U",
+  authDomain: "react-native-social-725000725.firebaseapp.com",
+  projectId: "react-native-social-725000725",
+  storageBucket: "react-native-social-725000725.appspot.com",
+  messagingSenderId: "57885908913",
+  appId: "1:57885908913:web:a59663734e462b116f6790",
+  measurementId: "G-BSHN25TR49",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebase;
